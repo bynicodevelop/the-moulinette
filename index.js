68bdb65d65026ee3d3b06ee1df8b9fc148252145
nico-develop <contact@nico-develop.fr>
2017-10-02 15:26:12 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = process.argv[2];
+
+if(filePath === undefined) {
+    throw new Error('Invalid path');
+}
+
+const goodFiles = [];
+const badFiles = [];
+
+fs.readdirSync(filePath).forEach(file => {
+
+    if(!file.match(/(^[A-Z][a-s]+_[A-Z][a-s]+_[A-Z0-9]+_[A-Z\-]+.([a-z0-9]{3,4}))/)) {
+        badFiles.push(file);
+    } else {
+        goodFiles.push(file);
+    }
+});
+
+console.log("Les mauvais fichiers sont :");
+badFiles.forEach(el => console.log(el));
+
+console.log("Les bons fichiers sont :");
+goodFiles.forEach(el => console.log(el));
